736d54e8c1b06b1d3b7632892d6020a141aac5f5
Rito Alpha <ritoalpha1843@gmail.com>
2019-11-01 12:07:58 +0700

    change config debug mode to true and change keys

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,8 @@
 		
        {{-- <h1>{{$title}}</h1>
        <p>white is bla bla bla balbla  bla balbla  bla balbla  bla balbla  bla balbla </ --}}
-       	<p>This is a project by CaoThienHuan-HTT2017</p>
+              <p>Disk D:\</p>
+       	<p>This is a project by CaoThienHuan-HTT2017 </p>
        	<p>
        		<a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
        		<a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
